a19bfb9e45c0d7ecad32b414b039d8b405b1d176
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-29 21:05:40 +0200

    Continue status message

--- tests/Feature/PostControllerTest.php
+++ tests/Feature/PostControllerTest.php
@@ -30,59 +30,64 @@ class PostControllerTest extends TestCase
         $response->assertJson([
             'status' => 201,
             'message' => 'The post was successfully created',
-            'data' => [
-                'title' => 'Test Post',
-                'body' => 'This is a test post.',
-                'author' => 'Moi même',
-            ],
+            'data' => $postData,
         ]);
 
         $this->assertDatabaseHas('posts', $postData);
     }
 
-    public function testCreateMultiPosts()
-    {
-        for ($i = 1; $i <= 10; $i++) {
-            $postData = [
-                'title' => 'Test Post ' . $i,
-                'body' => "This is a post number {$i}",
-                'author' => 'Moi même',
-            ];
+    // public function testCreateMultiPosts()
+    // {
+    //     $createdMultiPostsData = [];
 
-            $response = $this->postJson('/api/posts', $postData);
-            $response->assertStatus(201);
+    //     for ($i = 1; $i <= 10; $i++) {
+    //         $postData = [
+    //             'title' => 'Test Post ' . $i,
+    //             'body' => "This is a post number {$i}",
+    //             'author' => 'Moi même',
+    //         ];
 
-            $response->assertJson([
-                'message' => 'The post was successfully created',
-                'data' => $postData,
-            ]);
+    //         $response = $this->postJson('/api/posts', $postData);
+    //         $response->assertStatus(201);
 
-            $this->assertDatabaseHas('posts', $postData);
-        }
-    }
+    //         $response->assertJson([
+    //             'status' => 201,
+    //             'message' => 'The post was successfully created',
+    //             'data' => $postData,
+    //         ]);
+    //         $createdMultiPostsData[] = $postData;
+    //     }
+    //     foreach($createdMultiPostsData as $postData) {
+    //         $this->assertDatabaseHas('posts', $postData);
+    //     }
+
+    // }
 
     public function testUpdatePost()
     {
-
         $post = Post::factory()->create();
-
+    
         $updatedData = [
             'title' => 'Updated Title',
             'body' => 'Updated Body',
             'author' => 'Updated Author',
         ];
-
+    
         $response = $this->putJson('/api/posts/' . $post->id, $updatedData);
-
+    
+        dd($response->status(), $response->getContent());
+    
         $response->assertStatus(200);
-
+    
         $response->assertJson([
+            'status' => 200,
             'message' => 'The post was successfully updated',
             'data' => $updatedData,
         ]);
-
-        $this->assertDatabaseHas('posts', array_merge(['id' => $post->id], $updatedData));
+    
+        $this->assertDatabaseHas('posts', $updatedData);
     }
+    
 
     public function testDeletePost()
     {
@@ -91,10 +96,15 @@ class PostControllerTest extends TestCase
 
         $response = $this->deleteJson('/api/posts/' . $post->id);
 
+        if (!$post) {
+            $this->fail('Post not found');
+        }
+
         $response->assertStatus(200);
 
         $response->assertJson([
-            "message" => 'The post was successfully deleted',
+            'status' => 200,
+            'message' => 'The post was successfully deleted',
         ]);
 
         $this->assertDatabaseMissing('posts', ['id' => $post->id]);
