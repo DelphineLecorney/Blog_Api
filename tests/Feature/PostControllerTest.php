fff34863bc4cf30895cc50888c86722890f16647
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-07-29 14:01:23 +0200

    Add function update

--- tests/Feature/PostControllerTest.php
+++ tests/Feature/PostControllerTest.php
@@ -41,30 +41,58 @@ class PostControllerTest extends TestCase
     }
 
     public function testCreateMultiPosts()
-{
-    for ($i = 1; $i <= 10; $i++) {
-        $response = $this->postJson('/api/posts', [
-            'title' => 'Test Post ' . $i,
-            'body' => "This is a post number {$i}",
-            'author' => 'Moi même'
-        ]);
-        $response->assertStatus(201);
-
-        $response->assertJson([
-            'message' => 'The post was successfully created',
-            'data' => [
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $response = $this->postJson('/api/posts', [
                 'title' => 'Test Post ' . $i,
                 'body' => "This is a post number {$i}",
                 'author' => 'Moi même'
-            ]
+            ]);
+            $response->assertStatus(201);
+
+            $response->assertJson([
+                'message' => 'The post was successfully created',
+                'data' => [
+                    'title' => 'Test Post ' . $i,
+                    'body' => "This is a post number {$i}",
+                    'author' => 'Moi même'
+                ]
+            ]);
+
+            $this->assertDatabaseHas('posts', [
+                'title' => 'Test Post ' . $i,
+                'body' => "This is a post number {$i}",
+                'author' => 'Moi même',
+            ]);
+        }
+    }
+
+    public function testUpdatePost()
+    {
+
+        $post = Post::factory()->create();
+
+        $updatedData = [
+            'title' => 'Updated Title',
+            'body' => 'Updated Body',
+            'author' => 'Updated Author',
+        ];
+
+        $response = $this->putJson('/api/posts/' . $post->id, $updatedData);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'message' => 'The post was successfully updated',
+            'data' => [
+                'title' => 'Updated Title',
+                'body' => 'Updated Body',
+                'author' => 'Updated Author',
+            ],
         ]);
 
-        $this->assertDatabaseHas('posts', [
-            'title' => 'Test Post ' . $i,
-            'body' => "This is a post number {$i}",
-            'author' => 'Moi même',
-        ]);
+        $this->assertDatabaseHas('posts', $updatedData);
     }
-}
+
 
 }
