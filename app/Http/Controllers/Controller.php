6e5299b97d52c1719aa5d29c9311c1f2f18d7f3b
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-07-30 15:36:07 +0200

    Add function getPaginatedPosts in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Post;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function getPaginatedPosts()
+    {
+        $posts = Post::paginate(10);
+
+        return response()->json($posts);
+    }
 }
